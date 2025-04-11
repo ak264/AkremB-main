14b81461ad0f69bdd658753a9ca0ebf78dcca6b7
ak264 <55323623+ak264@users.noreply.github.com>
2025-04-10 23:21:42 -0500

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
-import { rss } from '@astrojs/rss';
+import { generateRSS } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
-	return rss({
+	return generateRSS({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
