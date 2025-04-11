956b979d5cbd40bf6fcef878186fff67819c42cd
ak264 <55323623+ak264@users.noreply.github.com>
2025-04-10 21:51:30 -0500

    Update site description to trigger a new deployment

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Akrem Berhanu';
-export const SITE_DESCRIPTION = 'Personal website of Akrem Berhanu';
+export const SITE_DESCRIPTION = 'Personal website and portfolio of Akrem Berhanu';
