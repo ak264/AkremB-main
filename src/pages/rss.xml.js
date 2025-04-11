3ba47c1316f4d1d66b7857c596d36e7ab9f89202
ak264 <55323623+ak264@users.noreply.github.com>
2025-04-10 23:32:12 -0500

    Fix RSS generation by updating to new API

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,10 @@
-import { generateRSS } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
-	return generateRSS({
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
