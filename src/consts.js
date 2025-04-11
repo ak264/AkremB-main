7bc309a33a408c3c54bea401215ca98794fd310b
ak264 <55323623+ak264@users.noreply.github.com>
2025-04-10 22:30:57 -0500

    Add missing SITE_TAGLINE export to consts.js

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Akrem Berhanu';
+export const SITE_TAGLINE = 'Technology Consultant & Engineer';
 export const SITE_DESCRIPTION = 'Personal website and portfolio of Akrem Berhanu';
